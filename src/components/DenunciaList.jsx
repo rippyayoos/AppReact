4cbd52b0e6da1c70d0caf1741568eb3b4b404fa2
Joaquin Cuadra <juaquin.cuadra@inacapmail.cl>
2025-07-07 16:46:26 -0300

    Se muestra estados de denuncias en colores

--- src/components/DenunciaList.jsx
+++ src/components/DenunciaList.jsx
@@ -7,7 +7,7 @@ export default function DenunciaList({ denuncias, onEliminar, onCambiarEstado })
       {denuncias.map((d) => (
         <div className="card mb-2" key={d.id}>
           <div className="card-body">
-            <h5>{d.tipo} - {d.estado}</h5>
+            <h5> {d.tipo} -{" "} <span className={`badge ${ d.estado === "pendiente" ? "bg-warning text-dark" : d.estado === "en revisión" ? "bg-info" : "bg-success" }`}> {d.estado} </span> </h5>
             <p><strong>Ubicación:</strong> {d.ubicacion}</p>
             <p>{d.descripcion}</p>
             <p><small>{d.fecha}</small></p>
